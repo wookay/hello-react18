206b5fc534ce00302a4a87b8ee2459919f1c37fd
WooKyoung Noh <wookay.noh@gmail.com>
2021-07-15 15:29:35 +0900

    init

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import ReactDOM from 'react-dom';
+import { screen, act } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
+
+test('renders learn react link', async () => {
+  const container = document.createElement('div');
+  document.body.appendChild(container);
+  const root = ReactDOM.createRoot(container);
+  await act(async () => {
+    root.render(<App />);
+  });
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
